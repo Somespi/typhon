24e4a172e6e2e70f4f146c329d7bb8251142fbde
Somespi <abdulalfarsi208@gmail.com>
2025-02-18 15:27:46 +0400

    implement tokens

---
+++ src/tokens/tokens.py
@@ -0,0 +1,27 @@
+from enums import Enum, auto
+from dataclasses import dataclass
+from typing import Tuple
+from position import Position
+
+class TokenType(Enum):
+    KEYWORD = auto()
+    IDENTIFIER = auto()
+    FLOAT = auto()
+    INTEGER = auto()
+
+    EQUAL = auto()
+    EQUAL_EQUAL = auto()
+    OPEN_PAREN = auto()
+    CLOSE_PAREN = auto()
+    OPEN_BRACE = auto()
+    CLOSE_BRACE = auto()
+    OPEN_BRACKET = auto()
+    CLOSE_BRACKET = auto()
+
+
+@dataclass 
+class Token:
+    position: Tuple[Position, Position]
+    type: TokenType 
+    literal: str
+    
