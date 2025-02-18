ae015f855428b3d709ba5d686f60bca93ad9344e
Somespi <abdulalfarsi208@gmail.com>
2025-02-18 15:39:13 +0400

    add position class

---
+++ src/tokens/position.py
@@ -0,0 +1,29 @@
+
+
+
+class Position:
+    def __init__(self, col: int, line: int, source: str):
+        self.col = col 
+        self.line = line
+        self.current = None
+        self.pos = -1 
+        self.source = source
+
+    def copy():
+        return self
+
+    def peek():
+        if self.pos + 1 < len(self.source):
+            self.pos += 1
+            self.current = self.source[self.pos]
+            if self.current == '\n':
+                self.line += 1
+                self.col   = 0
+            else:
+                self.col  += 1 
+        else:
+            self.current =  None
+
+    @property
+    def current():
+        return self.current
